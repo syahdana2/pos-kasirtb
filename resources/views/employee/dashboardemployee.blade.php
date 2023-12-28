f084fd76bfc35407662e9bc7524f9b5a93c598c6
odansyyy <odansyaharta26@gmail.com>
2023-12-28 18:46:18 +0700

    coba push lagi

--- resources/views/employee/dashboardemployee.blade.php
+++ resources/views/employee/dashboardemployee.blade.php
@@ -349,13 +349,6 @@
                     <td><span class="tag tag-primary">Approved</span></td>
                     <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>
                   </tr>
-                  <tr>
-                    <td>982</td>
-                    <td>Rocky Doe</td>
-                    <td>11-7-2014</td>
-                    <td><span class="tag tag-danger">Denied</span></td>
-                    <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>
-                  </tr>
                 </tbody>
               </table>
             </div>
